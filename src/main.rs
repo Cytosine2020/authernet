f73568aa13d5cbae87998650c4885ed71e2480ca
Cytosine1999 <34590769+Cytosine1999@users.noreply.github.com>
2020-09-11 00:36:30 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,84 @@
-use cpal::{Data, Sample, SampleFormat};
+use cpal::SampleFormat;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 
-fn write_silence<T: Sample>(data: &mut [T], _: &cpal::OutputCallbackInfo) {
-    for sample in data.iter_mut() {
-        *sample = Sample::from(&0.0);
+pub struct WaveGen {
+    t: u64,
+    t0: u64,
+    rate: u64,
+}
+
+impl WaveGen {
+    pub fn new(t0: u64, rate: u64) -> Self { Self { t: 0, t0, rate } }
+
+    pub fn calculate(&self) -> f32 {
+        ((self.t - self.t0) as f32 * 2f32 * std::f32::consts::PI / self.rate as f32).sin()
     }
 }
+
+impl Iterator for WaveGen {
+    type Item = i16;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let ret = (self.calculate() * i16::max_value() as f32) as i16;
+        self.t += 1;
+        if self.t > self.rate { self.t -= self.rate; }
+        Some(ret)
+    }
+}
+
+// pub struct Encoder<I, T> {
+//     iter: I,
+//     carrier: T,
+// }
+//
+// impl<I, T> Encoder<I, T> {
+//     pub fn new(iter: I, carrier: T) -> Self { Self { iter, carrier } }
+// }
+//
+// impl<I, T> Iterator for Encoder<I, T>
+//     where I: Iterator,
+//           I::Item: Into<bool>,
+//           T: Iterator,
+// {
+//     type Item = i32;
+//
+//     fn next(&mut self) -> Option<Self::Item> {
+//         match self.iter.next() {
+//             Some(item) => if item.into() { Some(1) } else { Some(0) },
+//             None => None,
+//         }
+//     }
+// }
+
 
 fn main() {
     let device = cpal::default_host().default_output_device().expect("no output device available");
 
     let supported_config = device
         .supported_output_configs().expect("error while querying configs")
-        .next().expect("no supported config?!")
+        .filter(|conf| conf.sample_format() == SampleFormat::F32)
+        .next().expect("expected sample format not found")
         .with_max_sample_rate();
 
-//     let stream = device.build_output_stream(
-//         &config,
-//         move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-// // react to stream events and read or write stream data here.
-//         },
-//         move |err| {
-// // react to errors here.
-//         },
-//     );
-
-    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
-    let sample_format = supported_config.sample_format();
     let config = supported_config.into();
-    let stream = match sample_format {
-        SampleFormat::F32 => device.build_output_stream(&config, write_silence::<f32>, err_fn),
-        SampleFormat::I16 => device.build_output_stream(&config, write_silence::<i16>, err_fn),
-        SampleFormat::U16 => device.build_output_stream(&config, write_silence::<u16>, err_fn),
-    }.unwrap();
+
+    let mut wave = WaveGen::new(0, 10);
+
+    // for item in wave {
+    //     println!("{}", item)
+    // }
+
+    let stream = device.build_output_stream(
+        &config,
+        move |data: &mut [f32], _| {
+            for sample in data.iter_mut() {
+                *sample = wave.next().expect("") as f32;
+            }
+        },
+        |err| {
+            eprintln!("an error occurred on the output audio stream: {}", err)
+        }).unwrap();
 
     stream.play().unwrap();
 }
