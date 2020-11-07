30f77d6bca204fb7ad3b552de3b7c1d8c24e765a
Kaiyuan Xu <xuky@shanghaitech.edu.cn>
2020-11-07 22:31:45 +0800

    improve frame interval

--- src/athernet.rs
+++ src/athernet.rs
@@ -11,11 +11,11 @@ use crate::{
 
 const ACK_TIMEOUT: usize = 1200;
 const BACK_OFF_WINDOW: usize = 128;
-const DIFS: usize = 48;
+const FRAME_INTERVAL: usize = 64;
 
 
 enum SendState<I> {
-    Idle,
+    Idle(usize),
     Sending(MacFrame, I, usize),
     WaitAck(MacFrame, usize, usize),
 }
@@ -39,11 +39,11 @@ impl Athernet {
     ) -> Result<(Sender<MacFrame>, Stream), Box<dyn std::error::Error>> {
         let (sender, receiver) = mpsc::channel();
 
-        let mut send_state = SendState::Idle;
+        let mut send_state = SendState::Idle(0);
         let mut buffer: Option<(MacFrame, usize, usize)> = None;
 
         let sending = move |frame: MacFrame, count| {
-            SendState::Sending(frame, std::iter::repeat(0).take(DIFS).chain(modulate(frame)), count)
+            SendState::Sending(frame, modulate(frame), count)
         };
 
         let back_off = move |frame: MacFrame, count: usize| {
@@ -63,14 +63,20 @@ impl Athernet {
                 *time = time.saturating_sub(data.len());
             }
 
-            if let SendState::WaitAck(_, ref mut time, _) = send_state {
-                *time = time.saturating_sub(data.len());
+            match send_state{
+                SendState::Idle(ref mut time) => {
+                    *time = time.saturating_sub(data.len());
+                }
+                SendState::WaitAck(_, ref mut time, _) => {
+                    *time = time.saturating_sub(data.len());
+                }
+                _ => {}
             }
 
             for sample in data.iter_mut() {
                 match send_state {
-                    SendState::Idle => {
-                        if channel_free {
+                    SendState::Idle(time) => {
+                        if channel_free && time == 0 {
                             if let Some((dest, tag)) = ack_send_receiver.try_iter().next() {
                                 let frame = MacFrame::new_ack(mac_addr, dest, tag);
                                 send_state = sending(frame, 0);
@@ -101,14 +107,14 @@ impl Athernet {
                                 send_state = if frame.is_data() && !frame.to_broadcast() {
                                     SendState::WaitAck(frame, ACK_TIMEOUT, count)
                                 } else {
-                                    SendState::Idle
+                                    SendState::Idle(0)
                                 }
                             }
                         } else {
                             if !frame.is_ack() {
                                 buffer = back_off(frame, count);
                             }
-                            send_state = SendState::Idle;
+                            send_state = SendState::Idle(0);
                         };
                     }
                     SendState::WaitAck(frame, ref mut time, count) => {
@@ -118,13 +124,13 @@ impl Athernet {
                             }) {
                                 bit_count += frame.get_payload_size() * 8;
 
-                                send_state = SendState::Idle;
+                                send_state = SendState::Idle(FRAME_INTERVAL);
                             } else {
                                 break;
                             };
                         } else {
                             buffer = back_off(frame, count);
-                            send_state = SendState::Idle;
+                            send_state = SendState::Idle(0);
                         };
                     }
                 };
